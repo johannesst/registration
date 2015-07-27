fc2ccb8c329386af85a579b32afee6839fcfbdcd
Johannes Starosta <j.starosta@tu-bs.de>
2015-07-27 08:19:06 +0200

    Show pending registrations only for admin users or moderators group

--- appinfo/app.php
+++ appinfo/app.php
@@ -10,9 +10,27 @@
  */
 
 namespace OCA\Registration\App;
+use \OCP\IUserSession;
 
 $app = new Registration();
 $c = $app->getContainer();
+
+
+// add an navigation entry
+$user = $c->getServer()->getUserSession()->getUser();
+$group = 'cleveradmin';
+if (\OC_Group::inGroup($user->getUID(), $group) || $c->isAdminUser() ){
+	$navigationEntry = function () use ($c) {
+		return [
+			'id' => $c->getAppName(),
+			'order' => 1,
+			'name' => $c->query('L10N')->t('Pending Requests'),
+			'href' => $c->query('URLGenerator')->linkToRoute('registration.register.pendingReg'),
+			'icon' => $c->query('URLGenerator')->imagePath('settings', 'users.svg'),
+		];
+	};
+	$c->getServer()->getNavigationManager()->add($navigationEntry);
+}
 
 \OC_App::registerLogIn(array('name' => $c->query('L10N')->t('Register'), 'href' => $c->query('URLGenerator')->linkToRoute('registration.register.askEmail')));
 
